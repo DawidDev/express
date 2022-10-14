6e862be4114c7bfb2e43ebea8bd30388fbb07005
Dawid <dawid.rozak.dev@gmail.com>
2022-10-14 12:18:32 +0200

    Create routing

--- app.js
+++ app.js
@@ -5,13 +5,32 @@ var cookieParser = require('cookie-parser'); // Wspiera w parsowaniu cookies
 var logger = require('morgan'); // służy do rzucenia logów w trybie developerskim
 
 var indexRouter = require('./routes/index'); // import strony startowej
-var usersRouter = require('./routes/users'); // import strony użytkownika
+var newsRouter = require('./routes/news'); 
+var quizRouter = require('./routes/quiz'); 
+var adminRouter = require('./routes/admin'); 
+
 
 var app = express();
+
+// Ustawienie nasłuchiwania
+const myPort = process.env.port || 3000;
+
+app.listen(myPort, '127.0.0.1', () => {
+    //console.log("Nasłuchujemy na porcie: " + myPort)
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); // katalog z widokami szablonami, template w pug'u
 app.set('view engine', 'pug');
+
+app.use(function(req, res, next){
+  
+  res.locals.path = req.path // Przekazujemy path do locals, globalnych danych w szablonach 
+
+  console.log(req.path) // przechowuje się tutaj aktualnie wybrany adres url
+
+  next() // Aby wywołać funkcję przy wywołaniu routingu tak robimy. Następnie dajemy Next() aby przekierowało do wybranego routingu
+})
 
 // Wywoływanie rozszerzeń które będziemy urzywać w Express'ie
 app.use(logger('dev')); 
@@ -21,7 +40,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler, przechwytywanie błędów routingów któe nie istnieją
 app.use(function(req, res, next) {
